de2710b37dd4ffcde98ba41dbc411c665343dd86
joaogcs <joaogcsoares1@gmail.com>
2020-11-08 20:32:03 -0300

    Fix quando o valor da parcela é inteiro

--- src/js/models/scraper.js
+++ src/js/models/scraper.js
@@ -11,8 +11,8 @@ class ColetarDaPagina {
     let lista_de_anuncios = [];
     // Para cada anuncio, coleta o ID
     $(".ui-search-layout > .ui-search-layout__item").each(function () {
-      let id = $(this).find("input[name=itemId]").val();
-      let anuncio = new Anuncio(id);
+      const id = $(this).find("input[name=itemId]").val();
+      const anuncio = new Anuncio(id);
       lista_de_anuncios.push(anuncio);
     });
     return lista_de_anuncios;
@@ -20,46 +20,44 @@ class ColetarDaPagina {
 
   static get_valor_total_parcelado_dos_resultados_da_busca(lista_de_anuncios) {
     // Para cada anuncio
-    $(".ui-search-layout > .ui-search-layout__item").each(function () {
+    $("li.ui-search-layout__item").each(function () {
       // id do anuncio
-      let id = $(this).find("input[name=itemId]").val();
-      let anuncio = lista_de_anuncios.find((anuncio) => anuncio.id === id);
+      const id = $(this).find("input[name=itemId]").val();
+      const anuncio = lista_de_anuncios.find((anuncio) => anuncio.id === id);
       // se parcelado com juros, calcula o total
       if (
         $(this).find(
           "span.ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK"
         ).length
       ) {
-        //pega o valor da parcela mostrado na tela
-        let valor_parcela = (
-          parseFloat(
-            $(this)
-              .find(
-                ".ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK"
-              )
-              .find("span.price-tag-fraction")
-              .text()
-          ) +
-          parseFloat(
-            $(this)
-              .find(
-                ".ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK"
-              )
-              .find("span.price-tag-cents")
-              .text()
-          ) /
-            100
-        ).toFixed(2);
 
-        let numero_parcelas = parseInt(
+        const el_inteiro = $(this)
+        .find(
+          ".ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK"
+        )
+        .find("span.price-tag-fraction");
+
+        const el_decimal = $(this)
+        .find(
+          ".ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK"
+        )
+        .find("span.price-tag-cents");
+
+        const valor_inteiro = $(el_inteiro).length > 0 ? parseInt($(el_inteiro).text()) : 0; 
+        
+        const valor_decimal = $(el_decimal).length > 0 ? parseFloat($(el_decimal).text()) : 0; 
+
+        const valor_parcela = parseFloat( valor_inteiro + (valor_decimal / 100)).toFixed(2);
+
+        const numero_parcelas = parseInt(
           $(this)
-            .find('div > div > div.ui-search-result__content-wrapper > div.ui-search-result__content-columns > div.ui-search-result__content-column.ui-search-result__content-column--left > div > span')
+            .find('.ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK')
               .text()
               .split(' ')[0]
               .replace(/\D/g, '')
         );
 
-        let valor_total = (valor_parcela * numero_parcelas).toFixed(2);
+        const valor_total = (valor_parcela * numero_parcelas).toFixed(2);
         anuncio.valor_total_parcelado = valor_total;
       }
     });
