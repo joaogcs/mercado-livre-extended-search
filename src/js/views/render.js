d6e21034db0220567c2719de8c6ed462c8b3e2c6
joaogcs <joaogcsoares1@gmail.com>
2020-11-15 12:45:20 -0300

    Renderizanod as opções de frete

--- src/js/views/render.js
+++ src/js/views/render.js
@@ -27,51 +27,124 @@ class Renderizar {
 
 
   static frete_preco (lista_de_anuncios) {
+
+    const this_class = this;
+
+    const margin_bottom = parseInt($(".ui-search-layout__item").css("margin-bottom").slice(0,-2));
+
     // Para cada anuncio
     $(".ui-search-layout > .ui-search-layout__item").each(function () {
+
       let id = $(this).find("input[name=itemId]").val();
       let anuncio = lista_de_anuncios.find((anuncio) => anuncio.id === id);
       
+      let linhas_adicionadas = 0;
+
       if (
         anuncio &&
         $(this).find(
           "div.ui-search-item__group__element.ui-search-item__group__element--shipping"
         ).length
       ) {
+
+        let linhas_adicionadas = 0;
+
         for (let opcao_de_entrega of anuncio.opcoes_de_entrega) {
 
           console.log ("if", anuncio);
 
           // renderiza o valor total da parcela na tela
           let valor_do_frete = document.createElement("p");
-          valor_do_frete.className = "ui-search-item__shipping ui-search-item__shipping--free";
-          valor_do_frete.innerHTML = "Frete grátis " + opcao_de_entrega.text;
+          if (opcao_de_entrega.preco === 0) {
+            valor_do_frete.className = "ui-search-item__shipping ui-search-item__shipping--free";
+            valor_do_frete.innerHTML = "Frete grátis " + opcao_de_entrega.text;
+          } else {
+            valor_do_frete.className = "ui-search-item__group__element ui-search-installments ui-search-color--BLACK ui-search-item__shipping ui-search-item__shipping";
+            valor_do_frete.innerHTML = "Frete " + "R$ " + opcao_de_entrega.preco + " " + opcao_de_entrega.text;
+          }
           $(this)
           .find(
             "p.ui-search-item__shipping.ui-search-item__shipping--free:last"
-            )
-            .after(document.createElement("br"))
+            ).after(document.createElement("br"))
+            .next()
             .after(valor_do_frete);
+
+          linhas_adicionadas++;
+
+          const new_margin_bottom = margin_bottom + linhas_adicionadas * 8;
+            $(".ui-search-layout__item")
+            .css("margin-bottom", new_margin_bottom + "px");
+
         }
+        $(this)
+          .find(
+            "p.ui-search-item__shipping.ui-search-item__shipping--free:first"
+          ).remove();
       } else {
+
+        $(this)
+      .find(
+        "div.ui-search-item__group.ui-search-item__group--price"
+        ).after(
+          this_class._createElement("div",
+          "ui-search-item__group ui-search-item__group--shipping",
+          "")
+        )
+        .next()
+        .append(
+          this_class._createElement("div",
+          "ui-search-item__group__element ui-search-item__group__element--shipping",
+          "")
+        )
+
+        let linhas_adicionadas = 0;
 
         for (let opcao_de_entrega of anuncio.opcoes_de_entrega) {
 
-          console.log ("else", anuncio);
-
           // renderiza o valor total da parcela na tela
           let valor_do_frete = document.createElement("p");
-          valor_do_frete.className = "ui-search-item__group__element ui-search-installments ui-search-color--BLACK ui-search-item__shipping ui-search-item__shipping--free";
-          valor_do_frete.innerHTML = "Frete " + opcao_de_entrega.text;
-          $(this)
-          .find(
-            "span.ui-search-item__group__element.ui-search-installments.ui-search-color--BLACK:last"
-            )
-            .after(document.createElement("br"))
-            .after(valor_do_frete);
+          valor_do_frete.className = "ui-search-item__group__element ui-search-installments ui-search-color--BLACK ui-search-item__shipping";
+          valor_do_frete.innerHTML = "Frete " + "R$ " + opcao_de_entrega.preco + " " + opcao_de_entrega.text;
+          
+          if ($(this).find("p.ui-search-item__shipping").length) {
+            
+            $(this)
+            .find(
+              "p.ui-search-item__shipping:last"
+              ).after(document.createElement("br"))
+              .next()
+              .after(valor_do_frete);
+            
+            linhas_adicionadas++;
+            
+            const new_margin_bottom = margin_bottom + linhas_adicionadas * 8;
+            $(".ui-search-layout__item")
+            .css("margin-bottom", new_margin_bottom + "px");
+            
+          } else {
+
+            $(this)
+            .find(
+              "div.ui-search-item__group__element.ui-search-item__group__element--shipping"
+              )
+              .append(valor_do_frete);
+
+              linhas_adicionadas++;
+          }
         }
       }
     });
   }
+
+  static _createElement(type, className, innerHTML) {
+    var element = document.createElement(type);
+    element.className = className;
+    if (innerHTML != "undefined") {
+      element.innerHTML = innerHTML;
+    }
+
+    return element;
+}
+
   
 }
